96c80b0a9d619a3ccf2fe55f53b9bd37f74d2b86
charlesworth <cochrane26@gmail.com>
2016-06-22 09:16:15 +0700

    initial commit, window that can be quit

---
+++ src/events.rs
@@ -0,0 +1,61 @@
+use sdl2::EventPump;
+
+pub struct Events {
+    pump: EventPump,
+
+    pub quit: bool,
+    // pub key_escape: bool,
+    pub up: bool,
+    pub down: bool,
+    pub left: bool,
+    pub right: bool,
+}
+
+impl Events {
+    pub fn new(pump: EventPump) -> Events {
+        Events {
+            pump: pump,
+
+            quit: false,
+            // key_escape: false,
+            up: false,
+            down: false,
+            left: false,
+            right: false,
+        }
+    }
+
+    /// Update the events record.
+    pub fn pump(&mut self) {
+        // If the SDL context is dropped, then poll_iter() will simply stop
+        // yielding any input.
+        for event in self.pump.poll_iter() {
+            use sdl2::event::Event::*;
+            use sdl2::keyboard::Keycode::*;
+
+            match event {
+                Quit { .. } => self.quit = true,
+
+                KeyDown { keycode, .. } => match keycode {
+                    // Some(Escape) => self.key_escape = true,
+                    Some(Up) => self.up = true,
+                    Some(Down) => self.down = true,
+                    Some(Left) => self.left = true,
+                    Some(Right) => self.right = true,
+                    _ => {}
+                },
+
+                KeyUp { keycode, .. } => match keycode {
+                    // Some(Escape) => self.key_escape = false,
+                    Some(Up) => self.up = false,
+                    Some(Down) => self.down = false,
+                    Some(Left) => self.left = false,
+                    Some(Right) => self.right = false,
+                    _ => {}
+                },
+
+                _ => {}
+            }
+        }
+    }
+}
